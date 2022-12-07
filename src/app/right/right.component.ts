b501c7e7c6548c0237620e3e093300009cdb464b
prabukdatasirpi <prabuk@datasirpi.com>
2022-12-07 15:09:06 +0530

    commit

--- src/app/right/right.component.ts
+++ src/app/right/right.component.ts
@@ -4,12 +4,16 @@ import { MyservicesService } from '../services/myservices.service';
 @Component({
   selector: 'app-right',
   templateUrl: './right.component.html',
-  styleUrls: ['./right.component.css']
+  styleUrls: ['./right.component.css'],
+  providers:[MyservicesService]
 })
 export class RightComponent {
   constructor(public _myServices: MyservicesService){
     console.log(_myServices.Counter);
-    
+
   }
 
+  Increase(){
+    this._myServices.addOne();
+  }
 }
